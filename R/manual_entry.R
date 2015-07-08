7f98c33bcd9cc37294c9faffb35eaed67165f72e
sara-02 <sarahmasud02@gmail.com>
2015-07-08 21:38:06 +0530

    Manual entry

---
+++ R/manual_entry.R
@@ -0,0 +1,10 @@
+ajay=c(22,56,78,89)
+View(ajay)
+ajay
+# View is the same as calling the object ajay, but View opens in a new window.
+edit(ajay)
+ajay
+#the edited value is not saved, anew temp object greated created instead.
+ajay=edit(ajay)
+ajay
+#Now this is what we wanted.
