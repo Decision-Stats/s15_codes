4a8e97b8d17c7019f9800f88dc641496dc221e2f
sara-02 <sarahmasud02@gmail.com>
2015-07-09 23:56:55 +0530

    string_basic

---
+++ R/string.R
@@ -0,0 +1,23 @@
+str="Hello, I am Sarah Masud"
+str
+# To find length of the string
+nchar(str)
+sl=tolower(str)
+su=toupper(str)
+su
+sl
+sp=strsplit(str,"Sa")
+sp
+cc=paste("Jim","and"," Jerry","are friends.")
+cc
+sprintf("The price of the  %s is $%d","Camera",5000)
+sub=substr(cc,start=4,stop=8)
+sub
+p="320"
+class(p)
+q=as.integer(p)
+class(q)
+pi=3.14
+class(pi)
+pi_s=as.character(pi)
+class(pi_s)
